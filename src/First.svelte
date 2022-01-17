7445b4140b9997571942621f76239cf255c3a036
mnxmnz <codi0703@naver.com>
2022-01-18 03:02:34 +0900

    Lecture 1. 선언적 렌더링

---
+++ src/First.svelte
@@ -0,0 +1,35 @@
+<script>
+	let name = 'World';
+	let age = 50;
+
+	function assign() {
+		name = 'Developer';
+		age = 30;
+	}
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<h2 class={age < 40 ? 'active' : ''}>{age}</h2>
+	<input type="text" bind:value={name}/>
+	<button on:click={assign}>Assign</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 10px;
+		max-width: 500px;
+		margin: 0 auto;
+	}
+
+	.active {
+		color: blue;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
