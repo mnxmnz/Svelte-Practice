def8bbcd2fbaa3701544774f534687501ca1dcfc
mnxmnz <codi0703@naver.com>
2022-01-25 01:36:57 +0000

    :truck: chore: 해당 lecture 폴더로 파일 이동

---
+++ src/Lecture03/App.svelte
@@ -0,0 +1,43 @@
+<script>
+  let name = 'World';
+  let isRed = false;
+
+  function enter() {
+    name = 'Enter';
+  }
+
+  function leave() {
+    name = 'Leave';
+  }
+
+  let text = 'Text';
+</script>
+
+<h1>Hello {name}</h1>
+<div
+  class="box"
+  style="background-color: {isRed ? 'red' : 'orange'}"
+  on:click={() => {
+    isRed = !isRed;
+  }}
+  on:mouseenter={enter}
+  on:mouseleave={leave}
+>
+  Click Box
+</div>
+
+<h1>{text}</h1>
+<input type="text" bind:value={text} />
+<button
+  on:click={() => {
+    text = 'Button';
+  }}>Click</button
+>
+
+<style>
+  .box {
+    width: 250px;
+    height: 150px;
+    margin: 0 0 30px;
+  }
+</style>
